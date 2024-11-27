eaaa2869547f9ea71e8f55d0947cd6e374fb7fed
buraito-kun <brightrock19220@outlook.co.th>
2024-11-27 17:08:50 +0700

    Add week 2 materials

---
+++ class/week2/client.py
@@ -0,0 +1,50 @@
+# Standard library
+import socket
+
+PORT = 12345
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(("localhost", PORT))
+
+while s.recv(4096).decode() != "ready":
+  print("Waiting for another player...")
+print("Other player has joined the room.")
+print("Game started!")
+
+choices_pool = ["rock", "paper", "scissor"]
+
+while True:
+  try:
+    player_action = input("""
+    Type 1, 2 or 3
+      1: (rock)
+      2: (paper)
+      3: (scissor)
+
+> """)
+    int(player_action)
+    if int(player_action) <= 0 and int(player_action) >= 4:
+      raise Exception("Error: specify value not in range 1-3.")
+    break
+  except:
+    pass
+
+s.send(str(choices_pool[int(player_action)-1]).encode())
+print("Opponent is thinking...")
+data = s.recv(4096).decode()
+if data != "finish":
+  s.close()
+  raise Exception("Invalid game logic order.")
+data = s.recv(4096).decode()
+if data == "draw":
+  print("Draw.")
+  print("Game ended.")
+elif data == "won":
+  print("You won!")
+  print("Game ended.")
+elif data == "lose":
+  print("You lose!")
+  print("Game ended.")
+
+s.close()
